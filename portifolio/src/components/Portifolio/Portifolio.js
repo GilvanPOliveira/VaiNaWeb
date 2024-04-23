88b2eda9bc198179a078a20bfb11552d8d53066d
Gilvan Oliveira <gilvanoliveira06@gmail.com>
2024-04-23 14:21:15 -0300

    :sparkles: Vai Na Web

--- portifolio/src/components/Portifolio/Portifolio.js
+++ portifolio/src/components/Portifolio/Portifolio.js
@@ -13,7 +13,6 @@ const projetos = [
     description:
       "Um breve resumo sobre mim, onde a proposta do desafio era criar um site com informações sobre mim, utilizando apenas HTML, e compartilhar com a turma para nos conhecermos melhor.",
     image: miniwiki,
-    // "https://i.imgur.com/0gGw96Q.png",
   },
   {
     id: 2,
@@ -21,7 +20,6 @@ const projetos = [
     description:
       "Clone da interface do Prime Video, onde a proposta do desafio era criar um site com a interface do Prime Video, utilizando apenas HTML e CSS, iniciando a utilização da responsividade.",
     image: primevideo,
-    // "https://i.imgur.com/ixXfwwv.png",
   },
   {
     id: 3,
@@ -29,7 +27,6 @@ const projetos = [
     description:
       "Beat, site de música, onde a proposta do desafio era criar um site com outras páginas integradas, novas estilizações, responsividade e organização do código, utilizando apenas HTML e CSS, ",
     image: beat,
-    // "https://i.imgur.com/LSCJ42n.png",
   },
   {
     id: 4,
@@ -37,7 +34,6 @@ const projetos = [
     description:
       "Foi proposto a criação de um site de cuidados pessoais, onde haveria venda de produtos naturais, vitaminas, perfumes, utilizando apenas HTML e CSS, aperfeiçoando a responsividade e organização, melhorando a estilização deixando a página mais atraente.",
     image: selfcare,
-    // "https://i.imgur.com/e7LtAiH.png",
   },
   {
     id: 5,
@@ -45,7 +41,6 @@ const projetos = [
     description:
       "Projeto de Natal, onde a proposta do desafio era criar um site com a temática natalina, referente a doações, utilizando apenas HTML e CSS, com a proposta de treinar tudo que foi estudado até o momento, responsividade, organização, estilização e novas técnicas.",
     image: natal,  
-    // "https://i.imgur.com/GgN4zoA.png",
   },
   {
     id: 6,
@@ -53,7 +48,6 @@ const projetos = [
     description:
       "Site criado para treinar a introdução ao JavaScript e React, trazendo todo conhecimento até então para essa nova linguagem e framework, utilizando HTML, CSS, JavaScript e React.",
     image: react,
-    // "https://i.imgur.com/K2YnoE5.png",
   },
   {
     id: 7,
@@ -61,7 +55,6 @@ const projetos = [
     description:
       "Clone da interface do McDonald's, onde a proposta do desafio era criar um site com a interface do McDonald's, utilizando apenas HTML, CSS, JavaScript e React, aprimorando a responsividade e organização do código, melorando a estilização da página, deixando-a mais atraente, prática e de fácil utilização(intuitiva).",
     image: mcdonalds,
-    // "https://i.imgur.com/z6CcYz8.png",
   },
 ];
 
